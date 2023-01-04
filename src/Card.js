d3271c7e0e615edba17ba742b964eacd4097fddc
jkumar0208 <jugeshkumar0208@gmail.com>
2023-01-04 16:48:17 +0100

    display the date and time

--- src/Card.js
+++ src/Card.js
@@ -1,4 +1,7 @@
 import IpLogo from "./ip-logo.png";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faClock, faCalendarAlt } from "@fortawesome/free-solid-svg-icons";
+import { DateTime } from "luxon";
 
 function Card({ data: { ip, region, city, country } }) {
     return (
@@ -12,6 +15,22 @@ function Card({ data: { ip, region, city, country } }) {
                 <div className="card-body">
                     <h6 className="card-title">Your IP address is {ip}</h6>
                     <p className="card-title">You are currently located in {city}, {country}</p>
+                </div>
+                <div className="card-footer pb-0 text-muted">
+
+                    <small>
+                        <p className="mb-0">
+                            <FontAwesomeIcon
+                                icon={faCalendarAlt}
+                                style={{ marginRight: "0.3em" }}
+                            />We are the {DateTime.local().toLocaleString()}
+                        </p>
+                        <p><FontAwesomeIcon
+                            icon={faClock}
+                            style={{ marginRight: "0.3em" }}
+                        />Your local time is {DateTime.local().toLocaleString(DateTime.TIME_SIMPLE)}
+                        </p>
+                    </small>
                 </div>
             </div>
         </div>
